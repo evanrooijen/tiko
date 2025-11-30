d3c0a9a57afd0d79abcff10e5a40eec595702726
Elmer van Rooijen <e.van.rooijen@hotmail.com>
2025-11-30 11:13:57 +0100

    feat(lefthook): Adds lefthook for git hooks
    Configures lefthook to run linting, formatting, and type checking before committing.
    
    This enhances code quality and consistency by automating these checks and preventing commits that don't pass.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import Providers from "./providers";
 
