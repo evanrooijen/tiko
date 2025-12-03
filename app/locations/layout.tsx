f20c869f933534a650daccf4ae5e84fe63cef410
Elmer van Rooijen <e.van.rooijen@hotmail.com>
2025-12-03 21:44:08 +0100

    Style: Removes underline from locations title
    Removes the underline from the "Locations" title for a cleaner look.

--- app/locations/layout.tsx
+++ app/locations/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ sidebar, main }: LayoutProps) => {
   return (
     <div className="py-10 px-2 sm:px-6 lg:px-8">
       <main className="container mx-auto flex flex-col gap-4">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance underline">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
           Locations
         </h1>
         <div className="grid grid-cols-[420px_1fr] gap-4">
